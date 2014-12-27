48ba7fcc7929d9b16dc83c28637f9343b3375d5d
Michael Brooks <mjbrooks@uw.edu>
2014-12-27 16:55:13 -0500

    update url configurations

--- message_coding/base/urls.py
+++ message_coding/base/urls.py
@@ -1,14 +1,14 @@
 """urlconf for the base application"""
 
-from django.conf.urls import url, patterns
-from django.contrib.auth.views import login
+from django.conf.urls import url, patterns, include
 
 
 urlpatterns = patterns('base.views',
-    url(r'^$', 'home', name='home'),
+                       url(r'^$', 'home', name='home'),
 )
 
-urlpatterns += patterns('', 
-	url(r'^login/$', 'django.contrib.auth.views.login'),
-	url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+urlpatterns += patterns('',
+                        url(r'^accounts/', include('django.contrib.auth.urls')),
+                        # url(r'^login/$', 'django.contrib.auth.views.login'),
+                        # url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
