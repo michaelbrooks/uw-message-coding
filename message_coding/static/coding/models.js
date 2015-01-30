c69cad97b266ed13584e46646192259deb111ba1
Michael Brooks <mjbrooks@uw.edu>
2015-01-30 10:00:27 -0800

    starting coding app

---
+++ message_coding/static/coding/models.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+
+    var module = angular.module('message_coding.dataBrowser.models', [
+        'message_coding.base.models'
+    ]);
+
+})();
---
+++ message_coding/static/coding/services.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    var module = angular.module('message_coding.dataBrowser.services', [
+        'message_coding.base.services'
+    ]);
+
+})();
