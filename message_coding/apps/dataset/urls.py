042b235588606620ade1e5870123e050b7adfa12
John Robinson <soco@uw.edu>
2015-05-03 20:36:23 -0700

    add exporting of multiple tasks

--- message_coding/apps/dataset/urls.py
+++ message_coding/apps/dataset/urls.py
@@ -6,20 +6,25 @@ import views
 
 urlpatterns = patterns('',
 
-                       url(r'^import/$',
-                           views.DatasetImportView.as_view(),
-                           name='dataset_import'),
+   url(r'^import/$',
+       views.DatasetImportView.as_view(),
+       name='dataset_import'),
 
-                       url(r'^(?P<dataset_slug>[\w-]+)/$',
-                           views.DatasetDetailView.as_view(),
-                           name='dataset'),
+   url(r'^(?P<dataset_slug>[\w-]+)/$',
+       views.DatasetDetailView.as_view(),
+       name='dataset'),
 
-                       url(r'^export/(?P<dataset_slug>[\w-]+)/$',
-                       		views.DatasetExportSelectionView.as_view(),
-                       		name='dataset-export'),
+   url(r'^export/(?P<dataset_slug>[\w-]+)/$',
+        views.DatasetExportSelectionView.as_view(),
+        name='dataset-export'),
 
-                       url(r'^download/(?P<dataset_slug>[\w-]+)/$',
-                       		views.DatasetExportView.as_view(),
-                       		name='dataset-download'),
+   url(r'^download/(?P<dataset_slug>[\w-]+)/$',
+        views.DatasetExportView.as_view(),
+        name='dataset-download'),
+
+    url(r'^download-tasks/(?P<dataset_slug>[\w-]+)/$',
+        views.DatasetTasksExportView.as_view(),
+        name='dataset-tasks-download'),
+
 
 )
--- message_coding/apps/dataset/views.py
+++ message_coding/apps/dataset/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import CreateView, DetailView, TemplateView, FormView, View
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, StreamingHttpResponse
+from django.http import HttpResponse, StreamingHttpResponse, HttpResponseRedirect
 
 from message_coding.apps.dataset import models, forms
 from message_coding.apps.coding import models as coding_models
@@ -13,11 +13,16 @@ from rest_framework.renderers import JSONRenderer
 
 import csv
 import codecs
+import urllib
+from datetime import datetime
 
 
 
 class DatasetDetailView(LoginRequiredMixin, ProjectViewMixin, DetailView):
-    """View for viewing datasets"""
+    """
+    View for viewing datasets
+    """
+
     model = models.Dataset
     template_name = 'dataset/dataset_detail.html'
     slug_url_kwarg = 'dataset_slug'
@@ -33,7 +38,9 @@ class DatasetDetailView(LoginRequiredMixin, ProjectViewMixin, DetailView):
 
 
 class DatasetImportView(LoginRequiredMixin, ProjectViewMixin, CreateView):
-    """View for importing a dataset """
+    """
+    View for importing a dataset
+    """
 
     form_class = forms.DatasetImportForm
     template_name = "dataset/dataset_import.html"
@@ -82,7 +89,9 @@ class DatasetImportView(LoginRequiredMixin, ProjectViewMixin, CreateView):
 
 
 class DatasetExportSelectionView(LoginRequiredMixin, ProjectViewMixin, FormView):
-    """ View to let user select which tasks to download """
+    """ 
+    View to let user select which tasks to download
+    """
 
     template_name = "dataset/dataset_export.html"
     form_class = forms.DatasetExportForm
@@ -103,9 +112,41 @@ class DatasetExportSelectionView(LoginRequiredMixin, ProjectViewMixin, FormView)
         return kwargs
 
 
+    def form_valid(self, form):
+        print "form valid - self: ", self.__dict__
+        print "form valid - form: ", form.__dict__
+        print "form tasks:", form["tasks"].__dict__
+        print "cleaned: ", form.cleaned_data["tasks"]
+        print "selected tasks: ", [t.id for t in form.cleaned_data["tasks"]]
+
+        dataset_slug = self.kwargs["dataset_slug"]
+        tasks = {"tasks[]": [t.id for t in form.cleaned_data["tasks"]] }
+        #print "form data: ", form_data
+        print urllib.urlencode(tasks)
+
+        return HttpResponseRedirect( "%s?%s"%(
+            reverse('dataset-tasks-download', kwargs={
+                "project_slug": self.get_project().slug,
+                "dataset_slug": dataset_slug} ), 
+            urllib.urlencode(tasks, doseq=True) ))
+        #return super(DatasetExportSelectionView, self).form_valid(form)
+
+    def get_success_url(self):
+        print "get success url __dict__:", self.__dict__
+        print "get success kwargs: ", self.kwargs
+        print "get success form: ", self.form
+        return reverse('export', kwargs={
+            'project_slug': self.get_project().slug,
+            'dataset_slug': self.object.slug,
+        })
+
+
+
 
 class DatasetExportView(LoginRequiredMixin, ProjectViewMixin, View):
-    """ Simple Export View """
+    """ 
+    Simple Export View 
+    """
 
     def get(self, request, project_slug, dataset_slug, **kwargs):
         #models.Message
@@ -142,3 +183,89 @@ class DatasetExportView(LoginRequiredMixin, ProjectViewMixin, View):
         # return the full response
         return response
 
+
+class Echo(object):
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+
+
+class DatasetTasksExportView(LoginRequiredMixin, ProjectViewMixin, View):
+    """
+    Task Selection View
+    """
+
+    def get(self, request, project_slug, dataset_slug, **kwargs):
+
+        message_set = set()
+        user_set = set()
+        code_set = set()
+
+        # grab the task ids from the get
+        task_ids = request.GET.getlist('tasks[]')
+        #print "task ids: ", task_ids
+        tasks = project_models.Task.objects.filter(id__in=task_ids)
+
+        # step through each task and find the messages, users, and codes
+        for task in tasks:
+            #print "task: ", task.id
+            message_ids = set(task.selection.get_messages().values_list("id", flat=True))
+            message_set |= message_ids
+
+            user_ids = set([ac.id for ac in task.assigned_coders.all()])
+            user_set |= user_ids
+
+            print "scheme"
+
+            code_objs = coding_models.Code.objects.filter(code_group__in=task.scheme.code_groups.all())
+            codes = set(code_objs.values_list("id", flat=True))
+            code_set |= codes
+
+        print "message_set: ", repr(message_set)
+        print "user_set: ", repr(user_set)
+        print "code_set: ", repr(code_set)
+
+        # begin a streaming response
+        response = HttpResponse(content_type="text/csv")
+        response['Content-Disposition'] = 'attachment; filename="%s_%s_%s.csv"'%(project_slug, dataset_slug, datetime.now().strftime("%Y%m%d-%H%M"))
+
+        # attach a csv writer to that
+        csvwriter = csv.writer(response)
+        csvwriter.writerow(["id", "time", "sender", "text", "codes_str"])
+
+        # create a utf-8 encoder
+        encoder = codecs.getencoder("utf-8")
+
+        # XXX: TODO
+        # this is insanely inefficient
+        message_list = sorted(message_set)
+        for msg_id in message_list:
+
+            msg_list = models.Message.objects.filter(id__exact=msg_id)
+            for m in msg_list:
+
+                # grab all instances attacahed to this 
+                instances = project_models.CodeInstance.objects.filter(message=m, owner__in=user_set, code__in=code_set )
+                codes_str =  u"|".join([unicode(c.code.name) for c in instances]) if instances is not None and instances.count() > 0 else ""
+
+                # create the column values for our row
+                cols = [unicode(m.id), unicode(m.time), unicode(m.sender),  unicode(m.text), codes_str]
+
+                # encode and write them
+                encoded_cols = [encoder(c)[0] for c in cols]
+                csvwriter.writerow(encoded_cols)
+
+        # return the full response
+        return response
+
+
+        # find our dataset & messages
+        #dataset = models.Dataset.objects.filter(slug=dataset_slug)
+        #messages = models.Message.objects.filter(dataset=dataset)
+
+
+        return HttpResponse("yay")
