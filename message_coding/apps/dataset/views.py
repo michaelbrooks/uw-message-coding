5acf5949715c0ba86609c05b63282bb886cd165f
John Robinson <soco@uw.edu>
2015-05-03 20:38:42 -0700

    remove the echo class

--- message_coding/apps/dataset/views.py
+++ message_coding/apps/dataset/views.py
@@ -184,15 +184,6 @@ class DatasetExportView(LoginRequiredMixin, ProjectViewMixin, View):
         return response
 
 
-class Echo(object):
-    """An object that implements just the write method of the file-like
-    interface.
-    """
-    def write(self, value):
-        """Write the value by returning it, instead of storing in a buffer."""
-        return value
-
-
 
 class DatasetTasksExportView(LoginRequiredMixin, ProjectViewMixin, View):
     """
